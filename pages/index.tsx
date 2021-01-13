73c726b3f22faf8263635e72005d491f6d82aff4
ArmandDu <dupuis.armand42@gmail.com>
2021-01-13 16:01:51 +0200

    chore: initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>AWS Transcribe with aws-sdk-js-v3 and NextJS</title>
+      </Head>
+
+      <main>AWS Transcribe Test</main>
+    </div>
+  );
+}
